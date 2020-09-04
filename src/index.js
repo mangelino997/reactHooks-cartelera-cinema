6aa9bbad7ee40231a2053cd1711bf5d232476413
Marcio <marcio@draimo.com>
2020-09-03 22:24:03 -0300

    Resolucion error router 4

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 //maneja el enrutado como un SPA single page aplication
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   //le decimos a React como mnejar el enrutado
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root')
 );
 
