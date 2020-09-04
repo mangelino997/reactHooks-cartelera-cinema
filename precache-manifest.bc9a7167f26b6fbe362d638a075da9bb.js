91d6a74ce187886ec38251f8121adb3f7352572e
Marcio <marcio@draimo.com>
2020-09-03 23:19:05 -0300

    Updates

--- precache-manifest.bc9a7167f26b6fbe362d638a075da9bb.js
+++ precache-manifest.bc9a7167f26b6fbe362d638a075da9bb.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9a9a105f3a9f9871bcc15906380ea11b",
+    "revision": "7c5851a54e281b1b52dc7401eba98873",
     "url": "/reactHooks-cartelera-cinema/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactHooks-cartelera-cinema/static/css/2.cd1899df.chunk.css"
   },
   {
-    "revision": "f8efe927b0391bbe1639",
+    "revision": "d1896e259bba57a5d065",
     "url": "/reactHooks-cartelera-cinema/static/css/main.fa2a8a57.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactHooks-cartelera-cinema/static/js/2.8cd507ec.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f8efe927b0391bbe1639",
-    "url": "/reactHooks-cartelera-cinema/static/js/main.4d442832.chunk.js"
+    "revision": "d1896e259bba57a5d065",
+    "url": "/reactHooks-cartelera-cinema/static/js/main.27b2ea70.chunk.js"
   },
   {
     "revision": "2537613900e19089e189",
