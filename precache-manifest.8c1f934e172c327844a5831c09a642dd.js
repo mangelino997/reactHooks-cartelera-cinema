8adffbd0c3a4395844bc9c1a2c6bede46c490e4a
Marcio <marcio@draimo.com>
2020-09-03 21:36:28 -0300

    Updates

---
+++ precache-manifest.8c1f934e172c327844a5831c09a642dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "906ca79cabe9631c50e20fbac3e6a2f7",
+    "url": "/reactHooks-cartelera-cinema/index.html"
+  },
+  {
+    "revision": "2787f2b0bfcf125854e5",
+    "url": "/reactHooks-cartelera-cinema/static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "1d9aafd16c57eb2de958",
+    "url": "/reactHooks-cartelera-cinema/static/css/main.fa2a8a57.chunk.css"
+  },
+  {
+    "revision": "2787f2b0bfcf125854e5",
+    "url": "/reactHooks-cartelera-cinema/static/js/2.2c4970db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactHooks-cartelera-cinema/static/js/2.2c4970db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d9aafd16c57eb2de958",
+    "url": "/reactHooks-cartelera-cinema/static/js/main.1169b06a.chunk.js"
+  },
+  {
+    "revision": "2537613900e19089e189",
+    "url": "/reactHooks-cartelera-cinema/static/js/runtime-main.dafd33ba.js"
+  },
+  {
+    "revision": "2a468ea8aa22b357bb4ff1e955a05265",
+    "url": "/reactHooks-cartelera-cinema/static/media/avatar.2a468ea8.png"
+  }
+]);
--- src/componentes/ButtonBackHome.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-
-export const ButtonBackHome = () => (
-    <Link to='/' className="button is-info">
-        Volver a Home
-    </Link>
-)
